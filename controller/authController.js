dbb3206accdef4a3f746f83201050dc4a739e5ed
yuriTakamisawaRibeiro <yuri.takamisawa.ribeiro@gmail.com>
2024-05-16 09:53:08 -0300

    crud authcontroller add

--- controller/authController.js
+++ controller/authController.js
@@ -2,11 +2,17 @@ const user = require('../db/models/user');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
+const cpfValidator = require('cpf-cnpj-validator').default;
+
 const generateToken = (payload) => {
     return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
         expiresIn: process.env.JWT_EXPIRES_IN
     });
 };
+
+const validateCpf = (cpf) => {
+    return cpfValidator.isValid(cpf);
+}
 
 const signup = async (req, res, next) => {
     const body = req.body;
@@ -19,6 +25,14 @@ const signup = async (req, res, next) => {
     }
 
     try {
+
+        if (!validateCpf(body.cpf)) {
+            return res.status(400).json({
+                status: 'fail',
+                message: 'CPF inválido.'
+            });
+        }
+        
         // Criando o novo usuário e esperando a promessa ser resolvida
         const hashedPassword = await bcrypt.hash(body.password, 10); // Criptografa a senha
         const newUser = await user.create({
@@ -50,6 +64,94 @@ const signup = async (req, res, next) => {
         });
     }
 };
+
+const updateUser = async (req, res, next) => {
+    const { id } = req.params; // Obtém o ID do usuário a partir dos parâmetros da requisição
+    const body = req.body; // Obtém os novos dados do corpo da requisição
+
+    try {
+        // Verifica se o usuário existe antes de tentar atualizá-lo
+        const existingUser = await user.findByPk(id);
+        if (!existingUser) {
+            return res.status(404).json({
+                status: 'fail',
+                message: 'Usuário não encontrado.'
+            });
+        }
+
+        // Prepara os campos para atualização
+        let fieldsToUpdate = {};
+        const updateableFields = ['name', 'email', 'password'];
+
+        updateableFields.forEach(field => {
+            if (body[field]) {
+                fieldsToUpdate[field] = body[field];
+            }
+        });
+
+        // Atualiza o usuário no banco de dados
+        const updatedUser = await user.update(fieldsToUpdate, { where: { id: id } });
+
+        if (updatedUser[0] === 1) {
+            // Retorna os dados atualizados do usuário
+            const updatedResult = await user.findByPk(id);
+            delete updatedResult.password; // Remover a senha do resultado para segurança
+            return res.status(200).json({
+                status: 'success',
+                data: updatedResult
+            });
+        } else {
+            return res.status(500).json({
+                status: 'fail',
+                message: 'Erro ao atualizar o usuário.'
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            status: 'fail',
+            message: 'Erro interno do servidor.'
+        });
+    }
+};
+
+
+const deleteUser = async (req, res, next) => {
+    const { id } = req.params; // Obtém o ID do usuário a partir dos parâmetros da requisição
+
+    try {
+        // Verifica se o usuário existe antes de tentar excluí-lo
+        const existingUser = await user.findByPk(id);
+        if (!existingUser) {
+            return res.status(404).json({
+                status: 'fail',
+                message: 'Usuário não encontrado.'
+            });
+        }
+
+        // Deleta o usuário do banco de dados
+        const deletedUser = await user.destroy({ where: { id: id } });
+
+        if (deletedUser > 0) {
+            return res.status(200).json({
+                status: 'success',
+                message: 'Usuário excluído com sucesso.'
+            });
+        } else {
+            return res.status(500).json({
+                status: 'fail',
+                message: 'Erro ao excluir o usuário.'
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            status: 'fail',
+            message: 'Erro interno do servidor.'
+        });
+    }
+};
+
 
 const login = async (req, res, next) => {
     const { email, password } = req.body;
@@ -83,4 +185,4 @@ const login = async (req, res, next) => {
 
 
 
-module.exports = { signup, login };
+module.exports = { signup, login, deleteUser, updateUser };
--- route/authRoute.js
+++ route/authRoute.js
@@ -1,12 +1,16 @@
 // rotas http
 
 const express = require('express');
-const { signup, login } = require('../controller/authController');
+const { signup, login, deleteUser, updateUser } = require('../controller/authController');
 
 const router = express.Router();
 
 router.route('/signup').post(signup);
 
 router.route('/login').post(login);
+
+router.route('deleteUser').delete(deleteUser);
+
+router.route('updateUser').put(updateUser);
 
 module.exports = router;
