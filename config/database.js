5cc76d60eb07e1ca553224bb7d5ac1e1fff5a6c6
yuriTakamisawaRibeiro <yuri.takamisawa.ribeiro@gmail.com>
2024-06-05 10:46:59 -0300

    update

--- config/database.js
+++ config/database.js
@@ -4,14 +4,17 @@ const config = require('./config');
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 
-if (!dbConfig.username ||!dbConfig.password ||!dbConfig.database ||!dbConfig.host) {
+// Verifica se todos os campos obrigatórios estão presentes, exceto a senha
+if ((!dbConfig.username &&!dbConfig.password) || 
+   !dbConfig.database || 
+   !dbConfig.host) {
     throw new Error('Database configuration is missing some required fields.');
-  }
+}
 
-  const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
     host: dbConfig.host,
     dialect: dbConfig.dialect,
     port: dbConfig.port,
-  });
+});
 
 module.exports = sequelize;
